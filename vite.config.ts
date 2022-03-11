d76d4c0fcf0dec1c0e1d2a5f2437e3c20cc619c3
xiaoqiyang <meetqy@qq.com>
2022-03-11 20:06:39 +0800

    chore: 消除警告

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ultraseven",
+  base: "/ultraseven/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
