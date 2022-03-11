160e244f5f62108fe7babe5e07d09620ce5e1712
xiaoqiyang <meetqy@qq.com>
2022-03-11 23:22:41 +0800

    feat: 增加dark模式

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,8 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: 'media', // or 'class'
   plugins: [require('@tailwindcss/typography'), require("daisyui")],
+  daisyui: {
+    themes: true,
+    darkTheme: 'light'
+  }
 }
