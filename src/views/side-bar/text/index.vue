384ca3471bed4cc57edd4e2797f4b0f6fc92e646
xiaoqiyang <meetqy@qq.com>
2022-03-15 14:39:36 +0800

    feat: add text color

--- src/views/side-bar/text/index.vue
+++ src/views/side-bar/text/index.vue
@@ -6,7 +6,7 @@
   >
     <input type="checkbox" class="peer" ref="collapse" />
     <div class="collapse-title text-xl font-medium">Text</div>
-    <div class="collapse-content">
+    <div class="collapse-content relative">
       <div class="border-t border-base-300 pt-4">
         <input
           type="text"
@@ -15,6 +15,22 @@
           v-model="innerText"
         />
       </div>
+
+      <div
+        class="form-control w-full px-4 max-w-xs h-96 absolute top-16 left-0"
+      >
+        <label class="label">
+          <span class="label-text">
+            文本颜色 （<span class="text-secondary">text-color</span>）
+          </span>
+        </label>
+        <ClassSelect
+          :options="options.color"
+          :model-value="value.color"
+          @update:model-value="(e) => changeValue(e, 'color')"
+        />
+      </div>
+      <div class="h-96"></div>
     </div>
   </div>
 </template>
@@ -23,7 +39,10 @@
 import { TextNode } from "@/constants";
 import { useBaseStore } from "@/stores/base";
 import type { CheckedElement } from "@/types";
-import { computed, ref, watch } from "vue";
+import { createOptions, usageClassFilter } from "@/views/utils";
+import { computed, reactive, ref, watch } from "vue";
+import type { ClassSelectOption } from "../components/class-select";
+import ClassSelect from "../components/class-select/index.vue";
 
 interface Props {
   element: CheckedElement;
@@ -54,4 +73,53 @@ watch(innerText, (val) => {
     });
   }
 });
+
+// 当前元素的所有class
+const classList = computed(() =>
+  props.element?.className ? props.element.className.split(" ") : []
+);
+
+const options = reactive<{
+  color: ClassSelectOption[];
+}>({
+  color: createOptions("text-color"),
+});
+
+interface ValueType {
+  color: string[];
+}
+
+const value = reactive<ValueType>({ color: [] });
+watch(classList, (val) => {
+  value.color = usageClassFilter(val, "text-color");
+});
+
+const changeValue = (modelValue: string[], name: keyof ValueType) => {
+  const temp = classList.value;
+  let currentValue = value[name];
+  const oldIndex = temp.indexOf(currentValue[0]);
+
+  if (oldIndex > -1) {
+    if (modelValue.length === 0) {
+      // 删除
+      temp.splice(oldIndex, 1);
+    } else {
+      // 替换
+      temp.splice(oldIndex, 1, modelValue[0]);
+    }
+  } else {
+    // 新增
+    temp.push(modelValue[0]);
+    console.log(temp, "111");
+  }
+
+  if (props.element) {
+    baseStore.updateCheckedElement({
+      ...props.element,
+      className: temp.join(" "),
+    });
+  }
+
+  value[name] = modelValue;
+};
 </script>
