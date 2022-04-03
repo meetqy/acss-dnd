67d7b69c11cfcf93c544972ecd5f500a04cde8e1
xiaoqiyang <meetqy@qq.com>
2022-04-03 23:15:09 +0800

    refactor: 💡 template用单独的package管理

--- src/template/index.tsx
+++ src/template/index.tsx
@@ -1,27 +1,47 @@
-import { initBreadcrumbs } from "./breadcrumbs";
-import { initCountdown } from "./countdown";
-import type { Menu } from "./d";
-// import { initFooter } from "./footer";
-import { initHero } from "./hero";
-import { initLayout } from "./layout";
-import { initText } from "./text";
 import * as ad_temp from "@acss-dnd/template";
 
+// ad_temp.breadcrumbs
+
 const initTemp = async (): Promise<Menu[]> => {
-  const menu: Menu[] = [];
-  // menu.push({
-  //   id: "Footer",
-  //   text: "Footer 底部说明",
-  //   wrapClass: [""],
-  //   components: temp.Footer,
-  // });
-  menu.push(await initText());
-  menu.push(await initLayout());
-  // menu.push(await initFooter());
-  menu.push(await initHero());
-  menu.push(await initBreadcrumbs());
-  menu.push(await initCountdown());
+  const menu: Menu[] = [
+    createMenu(ad_temp.breadcrumbs),
+    createMenu(ad_temp.countdown),
+    createMenu(ad_temp.footer),
+    createMenu(ad_temp.hero),
+    createMenu(ad_temp.text),
+  ];
+
   return menu;
+};
+
+interface Menu {
+  id: string;
+  text: string;
+  // 组件外部携带的className
+  // eg: 文本标签 h1 h2 p ... 外面的 prose,可以直接格式化其中的文本
+  wrapClass: string[];
+  components: ad_temp.Template[];
+}
+
+const createMenu = (module: ad_temp.T): Menu => {
+  const m: Menu = {
+    id: "",
+    text: "",
+    wrapClass: module.wrapClass || [],
+    components: [],
+  };
+  for (const i in module) {
+    const item = module[i];
+    if (i === "name") {
+      const name = item as string;
+      m.id = name.split(" ")[0];
+      m.text = name;
+    } else {
+      m.components.push(item as ad_temp.Template);
+    }
+  }
+
+  return m;
 };
 
 export default initTemp;
